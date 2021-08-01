ac96de13644092077c51c8aa034328ca83de0b88
gumeijun <751415315@qq.com>
2021-08-01 18:39:53 +0800

    first commit

---
+++ src/RegularCheck.php
@@ -0,0 +1,19 @@
+<?php
+namespace RegularCheck;
+class RegularCheck{
+    /**
+     * 校验手机号
+     * @param $mobile
+     * @return bool|void
+     */
+    public static function mobileCheck($mobile){
+        if (empty($mobile)) return;
+        if(preg_match("/^1[345789]\d{9}$/", $mobile)){
+            return true;
+        }
+        return false;
+    }
+    public static function IDcardCheck($idcard){
+        
+    }
+}
